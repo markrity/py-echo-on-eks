96fcc11256def53763dafd8c20980c3d553cfa76
Mark Davydov <markrity@gmail.com>
2021-03-19 21:21:12 +0200

    switched to other machine type

--- terraform/eks-worker-nodes.tf
+++ terraform/eks-worker-nodes.tf
@@ -44,7 +44,7 @@ resource "aws_eks_node_group" "demo" {
   node_role_arn   = aws_iam_role.demo-node.arn
   subnet_ids      = aws_subnet.demo[*].id
   
-  instance_types = ["t3.micro"]
+  instance_types = ["t3.small"]
   
   scaling_config {
     desired_size = 1
